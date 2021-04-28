870437a40b64c061b54cc6b0636bc8e6e906462e
Joshua Abshear <joshuaabshear@gmail.com>
2021-04-28 08:36:40 -0600

    first commit

--- roller.js
+++ roller.js
@@ -8,10 +8,26 @@ function buttonClicked() {
     var roll = getRandomInt(6)
     console.log( roll )
 
-    var node = document.createElement("TD");
-    var text = document.createTextNode( roll )
-    node.appendChild(text)
-    document.getElementById("rolls").appendChild(node)
+    var roll2 = getRandomInt(6)
+    console.log( roll2 )
+
+    document.getElementById("dice").src = "imgs/die-"+roll+".png" 
+    document.getElementById("dice2").src = "imgs/die-"+roll2+".png" 
+
+    if ( roll == roll2 ) {
+
+        document.getElementById("status").innerHTML = "Dice are Equal - DOUBLES!"
+
+    } else {
+
+        document.getElementById("status").innerHTML = "Player Rolled: " + (roll + roll2)
+
+    }
+
+    // var node = document.createElement("TD");
+    // var text = document.createTextNode( roll )
+    // node.appendChild(text)
+    // document.getElementById("rolls").appendChild(node)
 
 
     //document.getElementById("1").innerHTML = roll
