c2d9a4b4fc1c00bc99b70a2410d43263d56ca819
Joshua Abshear <joshuaabshear@gmail.com>
2021-04-30 12:05:18 -0600

    updates from Wednesday

--- roller.js
+++ roller.js
@@ -2,7 +2,29 @@ function getRandomInt(max) {
     return Math.floor(Math.random() * max) + 1;
 }
 
+function addDice() {
+
+    var node = document.createElement("IMG")
+    node.setAttribute("src", "imgs/die-1.png")
+    // set id attribute
+    document.getElementById("container").appendChild(node)
+
+}
+
+function removeDice() {
+
+    var container = document.getElementById("container");   // Get the containing element with id="myList"
+    container.removeChild(container.childNodes[0]);           // Remove <ul>'s first child node (index 0) 
+}
+
 function buttonClicked() {
+
+    addDice()
+
+}
+
+// Old Handler
+function buttonClicked2() {
     console.log("Roll was clicked")
 
     var roll = getRandomInt(6)
@@ -11,9 +33,11 @@ function buttonClicked() {
     var roll2 = getRandomInt(6)
     console.log( roll2 )
 
+    //Update Dice
     document.getElementById("dice").src = "imgs/die-"+roll+".png" 
     document.getElementById("dice2").src = "imgs/die-"+roll2+".png" 
 
+    //Check If Doubles
     if ( roll == roll2 ) {
 
         document.getElementById("status").innerHTML = "Dice are Equal - DOUBLES!"
@@ -23,14 +47,6 @@ function buttonClicked() {
         document.getElementById("status").innerHTML = "Player Rolled: " + (roll + roll2)
 
     }
-
-    // var node = document.createElement("TD");
-    // var text = document.createTextNode( roll )
-    // node.appendChild(text)
-    // document.getElementById("rolls").appendChild(node)
-
-
-    //document.getElementById("1").innerHTML = roll
     
 }
 
